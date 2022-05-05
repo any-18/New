e6118b71e7eb180b92b9a1e69234503b1cfd3dc7
Any <anylopezdev@gmail.com>
2022-05-04 20:41:53 -0500

    ahorasi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Newcomponent from './Newcomponent'
+
+function App() {
+  return (
+    <div className="App">
+      <Newcomponent></Newcomponent>
+      <h1>New App in React</h1>
+    </div>
+  );
+}
+
+export default App;
